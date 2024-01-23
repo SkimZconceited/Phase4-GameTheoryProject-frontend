79226b22c5637db884a4f53fbceb624fb77c225c
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2024-01-23 21:39:14 +0300

    Initial commit

---
+++ client/src/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+// import { useEffect, useState } from 'react';
+
+function Home() {
+  // const [info, setInfo] = useState({})
+
+  // useEffect(() => {
+  //     fetch('http://127.0.0.1:5555')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //     setInfo(data)
+  // })
+  // }, [])
+
+  // console.log(typeof(info))
+  // console.log(info.name)
+
+  return (
+    <div>
+      {/* <p>The data is: {info.name}</p> */}
+      <p>Hello web developer!</p>
+      <p>Dont delete the commented code it will be used to connect our server!</p>
+      
+    </div>
+  );
+}
+
+export default Home;
