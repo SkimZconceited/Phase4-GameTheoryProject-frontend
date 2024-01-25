652f4a8b19fcc4cd763e6b66ad250def2f275617
Conrad Kambi <kambiconrad@gmail.com>
2024-01-25 16:30:37 +0300

    Removed numbers from cells on the game board

--- client/src/components/Cell.jsx
+++ client/src/components/Cell.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Cell({ value, isDark }) {
+function Cell({ isDark }) {
   const cellStyle = {
     backgroundColor: isDark ? '#8B4513' : '#FFE4B5',
   };
 
-  return <div className={`cell ${isDark ? 'dark' : ''}`} style={cellStyle}>{value}</div>;
+  return <div className={`cell ${isDark ? 'dark' : ''}`} style={cellStyle}></div>;
 }
 
 export default Cell;
