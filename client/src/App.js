6c98b2e57a194934a14523abec3c40ac617e53c3
jim <jim.otieno@student.moringaschool.com>
2024-01-26 21:58:32 +0300

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Landing from './components/Landing';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Landing />} />
+        <Route path='/home' element={<Home/>}/>
       </Routes>
     </Router>
   );
